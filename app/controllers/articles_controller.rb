698f0b4d1c5e6f9b9c868b6f530196884731fff1
github username <github email address>
2019-05-07 17:18:49 -0400

    Built a show page. Like button logic completed, but unable to test yet.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,12 @@
 class ArticlesController < ApplicationController
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :author, :source, :summary, :topic, :content, :link, :img_url, :slant )
+  end
 end
